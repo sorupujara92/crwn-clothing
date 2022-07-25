edd4be739d0a2842c6c109c81e8d1be2a9f5726f
souravpujara27 <souravpujara@gmail.com>
2022-07-25 22:15:36 +0530

    reselect

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,16 @@
-export const selectCategoriesMap = (state) => state.categories.categories
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategory = createSelector(
+[selectCategoryReducer],
+(categoriesReducer) => categoriesReducer.categories
+);
+
+export const selectCategoriesMap = createSelector([selectCategory],(categories) => categories
 .reduce((acc, category) => {
     const { title, items } = category;
     acc[title.toLowerCase()] = items;
     return acc;
-  }, {});
+  }, {})
+);
